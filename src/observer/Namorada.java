b129680daa82e51d5ed8fbb85b3acf449a77d3b5
Gabriela Bowens <gabrielabowens@MacBook-Air-de-Gabriela.local>
2024-06-22 13:53:39 -0300

    tarefa feita

---
+++ src/observer/Namorada.java
@@ -0,0 +1,12 @@
+package observer;
+
+public class Namorada implements ChegadaAniversarianteObserver {
+
+	@Override
+	public void chegou(ChegadaAniversarianteEvent event) {
+		System.out.println("Apagar as luzes.");
+		System.out.println("Fazer Silencio.");
+		System.out.println("Surpresa!");
+	}
+
+}
